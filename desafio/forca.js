c5cfde6753584811ed587d175e7274c1679b1506
DanielaO85 <daniela.da.silva@gmail.com>
2022-07-24 18:44:10 -0300

    alteracao

--- desafio/forca.js
+++ desafio/forca.js
@@ -1,79 +1,78 @@
 
 class Forca {
 
-  //palavra = '';
-  letrasChutadas = []; // Deve conter todas as letras chutadas
-  vidas = 6;// Quantidade de vidas restantes
-  palavraOculta = [];
-  //palavraJogo = ['a', 'b', 'a', 'c', 'a', 'x', 'i']
+   
+    letrasChutadas = []; 
+    vidas = 6;
+    palavraOculta = [];
+    
 
 
-  constructor(palavra) {
-      this.palavra = palavra;
-      for (let i = 0; i < this.palavra.length; i++) {
-          this.palavraOculta.push('_')
-      }
-  }
+    constructor(palavra) {
+        this.palavra = palavra;
+        for (let i = 0; i < this.palavra.length; i++) {
+            this.palavraOculta.push('_')
+        }
+    }
 
 
-  chutar(chute) {
-      let encontrei = false;
+    chutar(chute) {
+        let encontrei = false;
 
-      if (chute.length !== 1) {
-          return 'aguardando chute'
-      }
+        if (chute.length !== 1) {
+            return 'aguardando chute'
+        }
 
-      for (let i = 0; i < this.letrasChutadas.length; i++) {
-          let comparar = this.letrasChutadas[i]
-          if (comparar === chute) {
-              return 'aguardando chute'
-          }
-      }
-
-
-      this.letrasChutadas.push(chute);
-
-      for (let i = 0; i < this.palavra.length; i++) {
-          const letra = this.palavra[i]
-
-          if (chute === letra) {
-              encontrei = true;
-              this.palavraOculta[i] = letra
-          }
-
-      }
-      if (encontrei === false) {
-          this.vidas = this.vidas - 1;
-      }
+        for (let i = 0; i < this.letrasChutadas.length; i++) {
+            let comparar = this.letrasChutadas[i]
+            if (comparar === chute) {
+                return 'aguardando chute'
+            }
+        }
 
 
-  }
+        this.letrasChutadas.push(chute);
+
+        for (let i = 0; i < this.palavra.length; i++) {
+            const letra = this.palavra[i]
+
+            if (chute === letra) {
+                encontrei = true;
+                this.palavraOculta[i] = letra
+            }
+
+        }
+        if (encontrei === false) {
+            this.vidas = this.vidas - 1;
+        }
+
+
+    }
 
 
 
-  buscarEstado() {
-      if (this.vidas === 0) {
-          return "perdeu"
-      }
+    buscarEstado() {
+        if (this.vidas === 0) {
+            return "perdeu"
+        }
 
-      if (this.palavraOculta.join('') === this.palavra) {
-          return "ganhou"
-      }
+        if (this.palavraOculta.join('') === this.palavra) {
+            return "ganhou"
+        }
 
-      return "aguardando chute"
-  }
-  // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
+        return "aguardando chute"
+    }
+   
 
-  buscarDadosDoJogo() {
-      return {
-          letrasChutadas: this.letrasChutadas, // Deve conter todas as letras chutadas
-          vidas: this.vidas,// Quantidade de vidas restantes
-          palavra: this.palavraOculta,
+    buscarDadosDoJogo() {
+        return {
+            letrasChutadas: this.letrasChutadas, 
+            vidas: this.vidas,
+            palavra: this.palavraOculta,
 
 
-          // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
-      }
-  }
+        }
+    }
 
 }
 
